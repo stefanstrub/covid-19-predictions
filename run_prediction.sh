4194a50d894bf14bb9466f25f16d84ceed4e4716
Stefan Strub <stefan.strub3@gmail.com>
2020-04-10 16:23:32 +0200

    first commit

---
+++ run_prediction.sh
@@ -0,0 +1,3 @@
+wget -N https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
+wget -N https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
+wget -N https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv
